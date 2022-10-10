107e1151112dcce44d2206b1e00f9ef00bc65bff
Rushikesh2189 <113567437+Rushikesh2189@users.noreply.github.com>
2022-10-11 00:01:21 +0530

    Demo
    Demo

---
+++ demo.js
@@ -0,0 +1,8 @@
+// Single line comment-This is my first program
+/* Multiline comment-Now I know two the two ways to run the prgram 
+Nodejs and Chrome enjine */
+console.log("Hello"); 
+console.log("My name is Rushikesh bhosale");
+console.info("This is info");    // info means information 
+console.warn("I am not that bad guy"); // warn means warning
+console.error("oops failed to execute"); 
